69f140a5a2951e2aafa4a94648e8eb5e5dff643f
Zainab Fatmi <syyeda.fatmi@cs.ox.ac.uk>
2024-09-20 18:17:26 +0100

    add coin example

---
+++ prism-examples/simple/coins/coins.pm
@@ -0,0 +1,28 @@
+dtmc
+
+module die
+
+	// local state
+	s : [0..8] init 8;
+	
+	[] s=8 -> 0.5 : (s'=3) + 0.5 : (s'=7);
+
+	[] s=0 -> 0.5 : (s'=1) + 0.5 : (s'=2); // c1
+	[] s=1 -> (s'=3); // head1
+	[] s=2 -> (s'=0); // tail1
+	[] s=3 -> 0.5 : (s'=1) + 0.5 : (s'=2); // c2
+
+	[] s=4 -> 0.5 : (s'=5) + 0.5 : (s'=6); // c3
+	[] s=5 -> (s'=7); // head2
+	[] s=6 -> (s'=4); // tail2
+	[] s=7 -> 0.5 : (s'=5) + 0.5 : (s'=6); // c4
+	
+endmodule
+
+rewards "pos"
+	s=1 | s=5 : 1;
+endrewards
+
+rewards "neg"
+	s=2 | s=6 : 1;
+endrewards
